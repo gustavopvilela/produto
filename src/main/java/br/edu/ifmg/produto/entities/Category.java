2ea1f6661e00820196fd85661f446a2a786abd5d
LucasAPV <lucasaugustopereiravilela@gmail.com>
2025-03-27 17:51:37 -0300

    aula 27-03

---
+++ src/main/java/br/edu/ifmg/produto/entities/Category.java
@@ -0,0 +1,58 @@
+package br.edu.ifmg.produto.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category(Long id, String name) {
+        this.setId(id);
+        this.setName(name);
+    }
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + this.getId() +
+                ", name='" + this.getName() + '\'' +
+                '}';
+    }
+}
