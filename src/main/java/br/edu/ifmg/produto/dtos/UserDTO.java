91ba871b2a601ee050e950f913d0d2b0e04d6570
gustavopvilela <gustavohp.vilela@gmail.com>
2025-05-15 17:43:36 -0300

    Aula 13 - CRUD de usuário

---
+++ src/main/java/br/edu/ifmg/produto/dtos/UserDTO.java
@@ -0,0 +1,88 @@
+package br.edu.ifmg.produto.dtos;
+
+import br.edu.ifmg.produto.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserDTO {
+    private Long id;
+    @NotBlank(message = "Campo obrigatório!")
+    private String firstName;
+    private String lastName;
+
+    @Email(message = "Favor informar um e-mail válido!")
+    private String email;
+    private Set<RoleDTO> roles = new HashSet<>();
+
+    public UserDTO() {}
+
+    public UserDTO(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public UserDTO (User user) {
+        this.id = user.getId();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+
+        user.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<RoleDTO> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<RoleDTO> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
+}
