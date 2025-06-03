6cba4fcab3aa9f5ae3e56075c2035fa0f592c852
gustavopvilela <gustavohp.vilela@gmail.com>
2025-06-03 15:25:44 -0300

    Aula 18 - Finalizando o endpoint paginado com filtro

--- src/main/java/br/edu/ifmg/produto/repository/ProductRepository.java
+++ src/main/java/br/edu/ifmg/produto/repository/ProductRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,27 +14,25 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query(nativeQuery = true, value = """
-        SELECT * FROM
-        (    
+        SELECT * FROM (
             SELECT DISTINCT p.id, p.name, p.image_url, p.price
             FROM tb_product p
             INNER JOIN tb_product_category pc ON pc.product_id = p.id
             WHERE
-                (:categoriesID IS NULL || pc.category_id in :categoriesID) AND
+                (:categoriesID IS NULL OR pc.category_id IN (:categoriesID)) AND
                 LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))
-        ) as tb_result
+        ) AS tb_result
     """,
     countQuery = """
-        SELECT COUNT(*) FROM
-        (
+        SELECT COUNT(*) FROM (
             SELECT DISTINCT p.id, p.name, p.image_url, p.price
             FROM tb_product p
             INNER JOIN tb_product_category pc ON pc.product_id = p.id
             WHERE
-                (:categoriesID IS NULL || pc.category_id in :categoriesID) AND
+                (:categoriesID IS NULL OR pc.category_id IN (:categoriesID)) AND
                 LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))
-        ) as tb_result
+        ) AS tb_result
     """)
-    /* O atributo countQuery não é necessário quando a consulta não é paginada. */
-    public Page<ProductProjection> searchProducts (List<Long> categoriesID, String name, Pageable pageable);
+    Page<ProductProjection> searchProducts(List<Long> categoriesID, String name, Pageable pageable);
+
 }
